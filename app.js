705c19a57072a48602f1e6fb7a66ed1ce5466ecd
= <treyyoung8@gmail.com>
2018-11-08 10:29:43 -0700

    completed

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ app.get('/calculate/:id', (req, res, next) => {
     next()
 })
 
+app.get('/calculate/:id/:secondId', (req, res, next) => {
+    let secondId = req.params.secondId
+    let id = req.params.id
+    let total = id / secondId
+    console.log(total)
+    res.send('Answer ' + total)
+    next()
+})
+
 app.get('/calculate/multiply/:id', (req, res, next) => {
     let id = req.params.id
     res.send(id)
@@ -39,7 +48,7 @@ app.get('/calculate/multiply/:id/:secondId', (req, res, next) => {
     let id = req.params.id
     let total = id * secondId
     console.log(total)
-    res.send({total})
+    res.send('Answer ' + total)
     next()
 })
 
@@ -54,7 +63,7 @@ app.get('/calculate/divide/:id/:secondId', (req, res, next) => {
     let id = req.params.id
     let total = id / secondId
     console.log(total)
-    res.send([total])
+    res.send('Answer ' + total)
     next()
 })
 
